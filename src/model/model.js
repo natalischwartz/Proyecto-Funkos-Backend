f351ee8fa12363a71d314eff936e195b8c52c6ad
rruizlopez <ruizlopezra@gmail.com>
2023-12-08 22:10:50 -0300

    Mejora en consulta de productos
    se cambia la expresion para consulta SQL para la obtencion de los productos, de modo tal de ver el nombre de la licencia y no su id

--- src/model/model.js
+++ src/model/model.js
@@ -4,7 +4,7 @@ const pool = require('../config/database');
 //obtener todos los productos de la base de datos 
 const getAllProductsFromDB = async () => {
     try {
-        const [productos] = await pool.query("SELECT * FROM product")
+        const [productos] = await pool.query("SELECT product.product_id, product.product_name, product.product_description,product.price,product.stock,product.sku,product.dues,product.image_front,product.image_back,product.create_time,licence.licence_name,product.category_id FROM product JOIN licence ON product.licence_id = licence.licence_id;")
         console.log("DATOS--->", productos)
         return productos;
     } catch (error) {
@@ -16,7 +16,7 @@ const getAllProductsFromDB = async () => {
 //Obtener un producto por ID desde BD
 const getItemByID = async (id) => {
     try {
-        const [dato] = await pool.query("SELECT * FROM product WHERE product_id = ?",[id]) 
+        const [dato] = await pool.query("SELECT * FROM product WHERE product_id = ?", [id])
         console.log("Producto--->", dato)
         return dato[0];
     } catch (error) {
